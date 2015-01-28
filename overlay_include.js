5bd678027d0dc23521e87eb5dd412e729e6a8e5e
t-kamitani <test.tkamitani@gmail.com>
2015-01-28 23:51:12 +0900

    Update overlay_include.js

--- overlay_include.js
+++ overlay_include.js
@@ -3,32 +3,37 @@
 	var timing = performance.timing || {};
 	var navigation = performance.navigation || {};
 	var res = '';
-	res = getDelta('navigationStart',timing);
-	res += getDelta('unloadEventStart',timing);
-	res += getDelta('unloadEventEnd',timing);
-	res += getDelta('redirectStart',timing);
-	res += getDelta('redirectEnd',timing);
-	res += getDelta('fetchStart',timing);
-	res += getDelta('domainLookupStart',timing);
-	res += getDelta('domainLookupEnd',timing);
-	res += getDelta('connectStart',timing);
-	res += getDelta('connectEnd',timing);
-	res += getDelta('requestStart',timing);
-	res += getDelta('responseStart',timing);
-	res += getDelta('responseEnd',timing);
-	res += getDelta('domLoading',timing);
-	res += getDelta('domInteractive',timing);
-	res += getDelta('domContentLoadedEventStart',timing);
-	res += getDelta('domContentLoadedEventEnd',timing);
-	res += getDelta('domComplete',timing);
-	res += getDelta('loadEventStart',timing);
-	res += getDelta('loadEventEnd',timing);
-    alert(res);
+	
+	// 全体表示
+	res += getDelta('page-loaded', 'navigationStart', 'loadEventEnd',timing);
+	
+	// サーバー再度
+	res = getDelta('networking', 'navigationStart', 'responseEnd', , ,timing);
+    res += getDelta('redirect', 'redirectStart', 'redirectEnd',timing);
+	res += getDelta('fetch', 'fetchStart', 'domainLookupStart',timing);
+	res += getDelta('dns', 'domainLookupStart', 'domainLookupEnd',timing);
+	res += getDelta('connect', 'connectStart', 'connectEnd',timing);
+	res += getDelta('request', 'requestStart', 'responseStart',timing);
+	res += getDelta('response', 'responseStart', 'responseEnd',timing);
+	res += getDelta('unloaded', 'unloadEventStart', 'unloadEventEnd',timing);
+	
+	// DOM構築全体
+	res += getDelta('dom-processing', 'domLoading', 'domComplete',timing);
+	// DOM構築内部処理1
+	res += getDelta('dom-loading', 'domLoading', 'domInteractive',timing);
+	// DOM構築内部処理2
+	res += getDelta('dom-content', 'domContentLoadedEventStart', 'domContentLoadedEventEnd',timing);
+	// DOM構築内部処理3
+	res += getDelta('dom-complete', 'domContentLoadedEventEnd', 'domComplete',timing);
+	
+	// ロードイベント開始
+	res += getDelta('loading', 'loadEventStart', 'loadEventEnd',timing);
 
-	function getDelta(endTiming, timing) {
-		var begin = timing['navigationStart'];
+    alert(res);
+	function getDelta(label,startTiming,endTiming, timing) {
+		var begin = timing[startTiming];
 		var end   = timing[endTiming]; 
-		if (!begin || !end) return endTiming + ":" + "n/a\n";
-		return endTiming + ":" + (end - begin) + "ms"+"\n";
+		if (!begin || !end) return label + ":" + "n/a\n";
+		return label + ":" + (end - begin) + "ms"+"\n";
 	}
 })();
