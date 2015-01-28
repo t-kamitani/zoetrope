889e013a22b0c47903f70deef85f099cf8a68599
t-kamitani <test.tkamitani@gmail.com>
2015-01-28 23:09:26 +0900

    Update overlay_include.js

--- overlay_include.js
+++ overlay_include.js
@@ -1,37 +1,34 @@
 (function(){
-	
-	window.performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {};
+    window.performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {};
 	var timing = performance.timing || {};
 	var navigation = performance.navigation || {};
-	res = JSON.stringify(timing, null, "  "); 
-
-	function getDelta(endTiming) {
-		var begin = Timing('navigationStart');
-		var end   = Timing(endTiming);
-		if (!begin || !end) return "n/a";
-		return endTiming ":" + (end - begin) + "ms¥n";
-	}
-	
 	var res = '';
-	res = getDelta('navigationStart');
-	res + getDelta('unloadEventStart');
-	res + getDelta('unloadEventEnd');
-	res + getDelta('redirectStart');
-	res + getDelta('redirectEnd');
-	res + getDelta('fetchStart');
-	res + getDelta('domainLookupStart');
-	res + getDelta('domainLookupEnd');
-	res + getDelta('connectStart');
-	res + getDelta('connectEnd');
-	res + getDelta('requestStart');
-	res + getDelta('responseStart');
-	res + getDelta('responseEnd');
-	res + getDelta('domLoading');
-	res + getDelta('domInteractive');
-	res + getDelta('domContentLoadedEventStart');
-	res + getDelta('domContentLoadedEventEnd');
-	res + getDelta('domComplete');
-	res + getDelta('loadEventStart');
-	res + getDelta('loadEventEnd');
+	res = getDelta('navigationStart',timing);
+	res += getDelta('unloadEventStart',timing);
+	res += getDelta('unloadEventEnd',timing);
+	res += getDelta('redirectStart',timing);
+	res += getDelta('redirectEnd',timing);
+	res += getDelta('fetchStart',timing);
+	res += getDelta('domainLookupStart',timing);
+	res += getDelta('domainLookupEnd',timing);
+	res += getDelta('connectStart',timing);
+	res += getDelta('connectEnd',timing);
+	res += getDelta('requestStart',timing);
+	res += getDelta('responseStart',timing);
+	res += getDelta('responseEnd',timing);
+	res += getDelta('domLoading',timing);
+	res += getDelta('domInteractive',timing);
+	res += getDelta('domContentLoadedEventStart',timing);
+	res += getDelta('domContentLoadedEventEnd',timing);
+	res += getDelta('domComplete',timing);
+	res += getDelta('loadEventStart',timing);
+	res += getDelta('loadEventEnd',timing);
     alert(res);
+
+	function getDelta(endTiming, timing) {
+		var begin = timing['navigationStart'];
+		var end   = timing[endTiming]; 
+		if (!begin || !end) return endTiming + ":" + "n/a\n";
+		return endTiming + ":" + (end - begin) + "ms"+"\n";
+	}
 })();
